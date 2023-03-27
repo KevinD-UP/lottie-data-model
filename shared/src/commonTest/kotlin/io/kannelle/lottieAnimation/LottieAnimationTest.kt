0ffa298455043b705ba7ec2a41def2088901d43f
Phetsana PHOMMARINH <phetsana@kannelle.io>
2023-03-27 17:48:03 +0200

    fix(test): update tests

--- shared/src/commonTest/kotlin/io/kannelle/lottieAnimation/LottieAnimationTest.kt
+++ shared/src/commonTest/kotlin/io/kannelle/lottieAnimation/LottieAnimationTest.kt
@@ -11,18 +11,35 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class LottieAnimationTest {
-    @Test
-    fun testDecode() {
-        val animationsToTest = listOf<String>(
-            "src/commonTest/resources/lottiefiles-carnival-archery.json",
-            "src/commonTest/resources/lottiefiles-cube-shape-animation.json",
-            "src/commonTest/resources/lottiefiles-wine-is-love.json",
-            "src/commonTest/resources/lottiefiles-scale-loop-animation.json",
-            "src/commonTest/resources/lottiefiles-car-rides.json"
+    fun animationsFor(theme: String): List<String> {
+        return listOf(
+            "src/commonTest/resources/animations/$theme/a.json",
+            "src/commonTest/resources/animations/$theme/b.json",
+            "src/commonTest/resources/animations/$theme/c.json",
+            "src/commonTest/resources/animations/$theme/c2.json",
+            "src/commonTest/resources/animations/$theme/d.json",
+            "src/commonTest/resources/animations/$theme/e.json",
+            "src/commonTest/resources/animations/$theme/f.json",
+            "src/commonTest/resources/animations/$theme/h.json",
+            "src/commonTest/resources/animations/$theme/i.json",
+            "src/commonTest/resources/animations/$theme/j.json",
+            "src/commonTest/resources/animations/$theme/k.json",
+            "src/commonTest/resources/animations/$theme/l.json",
+            "src/commonTest/resources/animations/$theme/m.json",
+            "src/commonTest/resources/animations/$theme/n.json",
+            "src/commonTest/resources/animations/$theme/o.json",
+            "src/commonTest/resources/animations/$theme/p.json",
+            "src/commonTest/resources/animations/$theme/q.json",
+            "src/commonTest/resources/animations/$theme/r.json",
+            "src/commonTest/resources/animations/$theme/s.json",
+            "src/commonTest/resources/animations/$theme/t.json",
         )
+    }
 
+    private fun animationsToTest(animationsToTest: List<String>) {
         animationsToTest.forEach {filename ->
             val path = filename.toPath()
+            println("path = $path")
             val animationJson = FileSystem.SYSTEM.read(path) {
                 readUtf8()
             }
@@ -36,7 +53,51 @@ class LottieAnimationTest {
             val outputJson = json.encodeToString(lottieAnimation)
             val outputAnimationElement = json.parseToJsonElement(outputJson)
             assertEquals(outputAnimationElement, animationElement)
+            println("path done = $path")
         }
+    }
+
+    @Test
+    fun testDecodeAlgiers() {
+        animationsToTest(animationsFor(theme = "algiers"))
+    }
+
+    @Test
+    fun testDecodeBali() {
+        animationsToTest(animationsFor(theme = "bali"))
+    }
+
+    @Test
+    fun testDecodeBerlin() {
+        animationsToTest(animationsFor(theme = "berlin"))
+    }
+
+    @Test
+    fun testDecodeGeneva() {
+        animationsToTest(animationsFor(theme = "geneva"))
+    }
+
+    @Test
+    fun testDecodeLosAngeles() {
+        animationsToTest(animationsFor(theme = "losAngeles"))
+    }
+
+    @Test
+    fun testDecodeSeattle() {
+        animationsToTest(animationsFor(theme = "seattle"))
+    }
+
+    @Test
+    fun testDecode() {
+        val animationsToTest = listOf<String>(
+            "src/commonTest/resources/lottiefiles-carnival-archery.json",
+            "src/commonTest/resources/lottiefiles-cube-shape-animation.json",
+            "src/commonTest/resources/lottiefiles-wine-is-love.json",
+            "src/commonTest/resources/lottiefiles-scale-loop-animation.json",
+            "src/commonTest/resources/lottiefiles-car-rides.json"
+        )
+
+        animationsToTest(animationsToTest)
     }
 }
 
