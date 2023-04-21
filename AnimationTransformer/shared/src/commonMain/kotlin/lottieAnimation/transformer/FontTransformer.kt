c83f2725aaccf1e6589f86cb89fcf2be65a1d3ce
Kévin DANG <kdang@pitchy.fr>
2023-04-21 14:08:33 +0200

    First implem of font transformer, need to write a unit test

--- AnimationTransformer/shared/src/commonMain/kotlin/lottieAnimation/transformer/FontTransformer.kt
+++ AnimationTransformer/shared/src/commonMain/kotlin/lottieAnimation/transformer/FontTransformer.kt
@@ -1,7 +1,11 @@
 package lottieAnimation.transformer
 
+import lottieAnimation.Font
+import lottieAnimation.FontList
 import lottieAnimation.LottieAnimation
-import lottieAnimation.layer.LayerType
+import lottieAnimation.layer.Layer
+import lottieAnimation.layer.TextData
+import lottieAnimation.layer.TextDocument
 import lottieAnimation.layer.TextLayer
 import lottieAnimation.rules.properties.AnimationRules
 
@@ -10,20 +14,37 @@ class FontTransformer {
     fun transformFonts(animation: LottieAnimation, animationRules: AnimationRules)
         : LottieAnimation
     {
-        var copyAnimation: LottieAnimation = animationRules.copy()
-
+        var res = animation.copy()
         animationRules.layerRules.forEach { layerRule ->
             if (layerRule.fontKey != null) {
-                val font = animation.fonts?.list?.find { it.fName == layerRule.fontKey }
-                val layer = animation.layers.find { it.ind == layerRule.ind }
-                if (font != null && layer != null) {
-                    when(layer) {
-                        is TextLayer -> {  }
+                val font = parseFontKey(layerRule.fontKey)
+                val wantedLayer = animation.layers.find { it.ind == layerRule.ind }
+                if (wantedLayer != null) {
+                    when(wantedLayer) {
+                        is TextLayer -> {
+                            val fontList: List<Font> = res.fonts!!.list!!.plus(font)
+                            val layers = res.layers.toMutableList()
+                            val index = layers.indexOf(wantedLayer)
+                            val newTextDocument : TextDocument = wantedLayer.t.d.k[0].s.copy(f = font.fName)
+                            val newKzero = wantedLayer.t.d.k[0].copy(s = newTextDocument)
+                            val newK = wantedLayer.t.d.k.toMutableList()
+                            newK[0] = newKzero
+                            val newD = wantedLayer.t.d.copy(k = newK)
+                            val newTextData : TextData = wantedLayer.t.copy(d = newD)
+                            val newLayer: TextLayer = wantedLayer.copy(t = newTextData)
+                            layers[index] = newLayer
+                            res = res.copy(fonts = FontList(fontList), layers = layers)
+                        }
                         else -> {}
                     }
                 }
             }
         }
-        return animation
+        return res
+    }
+
+    private fun parseFontKey(fontKey: String): Font {
+        val splittedFontKey = fontKey.split('-')
+        return Font(fFamily = splittedFontKey[0], fStyle = splittedFontKey[1], fName = fontKey)
     }
 }
