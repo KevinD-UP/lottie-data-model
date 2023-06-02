0c49c76d8b389fc9efe4672129b629e41169b8ef
Phetsana Phommarinh <pphommar@gmail.com>
2023-06-02 15:22:26 +0200

    refacto(animation-transformer): demo app text name

--- AnimationTransformer/iosApp/iosApp/ContentView.swift
+++ AnimationTransformer/iosApp/iosApp/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
                     Text("Expression")
                 }
                 NavigationLink(destination: FontTransformerView()) {
-                    Text("FontTransformer")
+                    Text("AnimationTransformer")
                 }
             }
         }
