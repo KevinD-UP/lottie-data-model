c432edc525b258711761e03315c6629df0148b90
Kévin DANG <kdang@pitchy.fr>
2023-06-15 16:36:22 +0200

    feat(): web project test using animationTransformer built with gradle.

---
+++ web-test/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+const animationTransformer = require('./script/kmp_lib')
+function App() {
+
+  const example = new animationTransformer.io.kannelle.testkmp.AnimationTransformerExample();
+  const text = example.animText()
+  const animJson = example.animJsonString(1)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {text}
+        <br></br>
+        <br></br>
+        <br></br>
+        {animJson}
+      </header>
+    </div>
+  );
+}
+
+export default App;
