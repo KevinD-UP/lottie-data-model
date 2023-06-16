7c501b9aaa6b613847ce909fc6f4c921886750e2
Kévin DANG <kdang@pitchy.fr>
2023-06-16 17:10:00 +0200

    feat(): show a Lottie Animation sent by Animation Transformer

--- web-test/src/App.tsx
+++ web-test/src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect }  from 'react';
+import Lottie from "lottie-web"
 import './App.css';
+
 const animationTransformer = require('./script/kmp_lib')
+
 function App() {
 
-  const example = new animationTransformer.io.kannelle.testkmp.AnimationTransformerExample();
-  const text = example.animText()
-  const animJson = example.animJsonString(1)
+    const example = new animationTransformer.io.kannelle.testkmp.AnimationTransformerExample();
+    const text = example.animText()
+    const animJson = example.animJsonString(1)
+
+    console.log(animationTransformer)
+
+    useEffect(() => {
+        Lottie.loadAnimation({
+            // @ts-ignore
+            container: document.querySelector('#animation'),
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            animationData: JSON.parse(animJson),
+        });
+    }, [animJson]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {text}
-        <br></br>
-        <br></br>
-        <br></br>
-        {animJson}
-      </header>
-    </div>
+    <div id={'animation'}></div>
   );
 }
 
