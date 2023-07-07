01fe39ee57e1c8d8342eb89f33e7d22d0e1a25e4
Kévin DANG <kdang@pitchy.fr>
2023-07-07 12:34:28 +0200

    feat(): testing front

--- web-test/src/App.tsx
+++ web-test/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react';
+import React, {useRef, useState} from 'react';
 import Lottie from "lottie-web"
 import './App.css';
 import {FunctionDelegate} from "./FunctionDelegate";
@@ -8,39 +8,75 @@ import * as rules from "./resource/BALI-FORD-rules.json"
 const animationTransformerLib = require('./script/kmp_lib')
 
 function App() {
-
-    const example = new animationTransformerLib.io.kannelle.testkmp.AnimationTransformerExample();
-    const animJson = example.animJsonString(1)
-
-    console.log(animationTransformerLib)
-
     const animationTransformer = new animationTransformerLib.transformer.KPAnimationTransformer(new FunctionDelegate());
-    console.log(animationTransformer)
 
+    const animationContainerRef = useRef(null);
     const lottieAnimationString: string = JSON.stringify(animation);
     const animationRulesString: string = JSON.stringify(rules);
 
-    const colors = {
-        text: '#FFFFF',
-        shape: '#000000',
-        background: '#000000'
-    }
+    const [textColor, setTextColor] = useState('#FFFFFF')
+    const [shapeColor, setShapeColor] = useState('#000000')
+    const [backgroundColor, setBackgroundColor] = useState('#000000')
 
-    const animTransformJson = animationTransformer.transformJs(lottieAnimationString, animationRulesString, null, null, colors)
 
-    useEffect(() => {
+    function transform() {
+        const colors = {
+            text: textColor,
+            shape: shapeColor,
+            background: backgroundColor
+        }
+
+        const font = {
+            textBold: 'Lato-Bold'
+        }
+
+        const animationContainer = animationContainerRef.current;
+
+        // Remove existing animation
+        // @ts-ignore
+        animationContainer.innerHTML = '';
+
+        const animTransformJson = animationTransformer.transformJs(lottieAnimationString, animationRulesString, null, font, colors)
         Lottie.loadAnimation({
             // @ts-ignore
-            container: document.querySelector('#animation'),
+            container: animationContainer,
             renderer: "svg",
             loop: true,
             autoplay: true,
             animationData: JSON.parse(animTransformJson),
         });
-    }, [animTransformJson]);
+    }
 
-  return (
-    <div id={'animation'}></div>
+    return (
+    <>
+        <div ref={animationContainerRef} id={'animation'}></div>
+        <span>textColor</span>
+        <input
+            key={`textColor`}
+            name="textColor"
+            value={textColor}
+            onChange={(e) => setTextColor(e.target.value)}
+        />
+        <span>shapeColor</span>
+        <input
+            key={`shapeColor`}
+            name="shapeColor"
+            value={shapeColor}
+            onChange={(e) => setShapeColor(e.target.value)}
+        />
+        <span>backgroundColor</span>
+        <input
+            key={`backgroundColor`}
+            name="backgroundColor"
+            value={backgroundColor}
+            onChange={(e) => setBackgroundColor(e.target.value)}
+        />
+        <button
+            onClick={() => transform()}
+        >
+            <span>transform</span>
+        </button>
+    </>
   );
 }
 
--- web-test/src/FunctionDelegate.ts
+++ web-test/src/FunctionDelegate.ts
@@ -1,28 +1,70 @@
 export class FunctionDelegate {
     getAscent(text: string, fontName: string, fontSize: number): number {
-        return 0
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        if (!context) return 0;
+
+        context.font = `${fontSize}px ${fontName}`;
+        console.log('context.font', context.font)
+        console.log('context', context)
+        const metrics = context.measureText(text);
+        console.log('metrics', metrics)
+        const ascent = metrics.fontBoundingBoxAscent;
+
+        return 109.596;
     }
 
     getDescent(text: string, fontName: string, fontSize: number): number {
-        return 0
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        if (!context) return 0;
+
+        context.font = `${fontSize}px ${fontName}`;
+        const metrics = context.measureText(text);
+        const descent = metrics.fontBoundingBoxDescent;
+
+        return descent;
     }
 
     getLastLineBottom(text: string, fontName: string, fontSize: number): number {
-        return -17.04
+        // Implémentation pour calculer le dernier ligne bottom réel
+        // Utilise les fonctionnalités de mesure de texte de JavaScript
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        if (!context) return -17.04;
+
+        context.font = `${fontSize}px ${fontName}`;
+        const metrics = context.measureText(text);
+        const descent = metrics.fontBoundingBoxDescent;
+
+        // Ajoute également la descente pour obtenir le dernier ligne bottom
+        const lastLineBottom = -descent;
+
+        return lastLineBottom;
     }
 
-    getTextMeasureHeight(
-        text: string,
-        fontName: string,
-        fontSize: number,
-        layerSize: number[],
-        layerLineHeight: number,
-        layerTracking: number
-    ): number {
-        return 129.6
+     getTextMeasureHeight(text: string, fontName: string, fontSize: number, layerSize: number[], layerLineHeight: number, layerTracking: number): number {
+         const canvas = document.createElement("canvas");
+         const context = canvas.getContext("2d");
+         if (!context) return 0;
+
+         context.font = `${fontSize}px ${fontName}`;
+         const metrics = context.measureText(text);
+         const fontHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
+
+         return 129.6
     }
+
 
     getTextLayerWidth(text: string, fontName: string, fontSize: number): number {
-        return 254.16
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        if (!context) return 254.16;
+
+        context.font = `${fontSize}px ${fontName}`;
+        const metrics = context.measureText(text);
+        const width = metrics.width;
+
+        return width;
     }
 }
